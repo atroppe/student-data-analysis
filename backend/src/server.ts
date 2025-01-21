4814c6fe0c0b186d61e6cb8446ba457a08df19c6
Amy Troppe <cmephro@aol.com>
2025-01-20 18:23:24 -0600

    add server info

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import csvRoutes from "./routes/apiRoutes";
 import apiRoutes from "./routes/apiRoutes";
+import path from 'path';
 
 const app = express();
 const PORT = 8080;
@@ -14,6 +15,15 @@ app.use(express.json());
 
 // Use the CSV routes
 app.use("/api", apiRoutes);
+
+
+
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
